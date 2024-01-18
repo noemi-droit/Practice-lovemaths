98fd4a2983e38c45e7c3b2a6bf98d41af899b8b0
noemi-droit llampa <noemillampa50@gmail.com>
2024-01-18 12:01:47 +0000

    Add all changes to html, css, script.js

---
+++ assets/css/js/script.js
@@ -0,0 +1,35 @@
+Binary file addedBIN +2.42 KB 
+02-adding-some-javascript/01-adding-structure/assets/images/logo.png
+
+ 31 changes: 31 additions & 0 deletions31  
+02-adding-some-javascript/01-adding-structure/assets/js/script.js
+@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
