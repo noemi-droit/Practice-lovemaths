5c1f14ff0312a071665822441544faa30c7a14f9
noemi-droit llampa <noemillampa50@gmail.com>
2024-01-18 12:22:52 +0000

    save changes to css, html and js

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,9 +1,4 @@
-Binary file addedBIN +2.42 KB 
-02-adding-some-javascript/01-adding-structure/assets/images/logo.png
 
- 31 changes: 31 additions & 0 deletions31  
-02-adding-some-javascript/01-adding-structure/assets/js/script.js
-@@ -0,0 +1,31 @@
 function runGame() {
 
 }
@@ -32,4 +27,4 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion() {}
