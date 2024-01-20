7c9d3e56f01f40266eff44e4ffe5a769c7ff59be
noemi-droit llampa <noemillampa50@gmail.com>
2024-01-20 00:37:44 +0000

    save changes to html,css, Javascript

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,33 +1,3 @@
-
-function runGame() {
-
-}
-
-function checkAnswer() {
-
-}
-
-function calculateCorrectAnswer() {
-
-}
-
-function incrementScore() {
-
-}
-
-function incrementWrongAnswer() {
-
-}
-
-function displayAdditionQuestion() {
-
-}
-
-function displaySubtractQuestion() {
-
-}
-
-function displayMultiplyQuestion() {}
-
+console.log("Connected!")
 
 
