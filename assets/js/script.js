e19bc76130bf65c960917c4aca87572a0b0a05e9
noemi-droit llampa <noemillampa50@gmail.com>
2024-01-22 20:50:50 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,8 +110,11 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
 
 function displayMultiplyQuestion() {
