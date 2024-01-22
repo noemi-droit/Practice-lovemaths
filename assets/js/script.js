378f8e7fb2926a847c64e6ae0bd830e7de68619d
noemi-droit llampa <noemillampa50@gmail.com>
2024-01-22 19:35:30 +0000

    Save changes to calculateCorrectAnswer

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,28 @@ function checkAnswer() {
 
 }
 
+ /**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ 
+ */
 function calculateCorrectAnswer() {
+    
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    } else if (operator ==="/") {
+		return [operand1 / operand2, "division"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
 
 }
 
@@ -55,7 +76,7 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
-    
+
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
